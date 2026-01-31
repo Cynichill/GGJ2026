e41e5e49ac854eb12f0518734d0ca6e6c6dbfb35
Quinn <kyled073@gmail.com>
2026-01-31 23:50:41 +0000

    TrapFactory corrected placement
    traps now snap to a division

--- trapFactory.gd
+++ trapFactory.gd
@@ -1,11 +1,21 @@
 extends Node
 @onready var trapScene = preload("res://Scenes/trap.tscn")
 
+const GRID_CORRECTION = 32
+
 func _ready():
 	EventBus.createTrap.connect(newTrap)
 
 func newTrap(creator, position):
 	var thisTrap: Trap = trapScene.instantiate()
 	thisTrap.creator = creator
-	thisTrap.position = Vector2(floor(position.x), floor(position.y))
+	thisTrap.position = correctPlacement(position)
 	add_child(thisTrap)
+	
+func correctPlacement(position):
+	var x = int(position.x / GRID_CORRECTION)
+	var y = int(position.y / GRID_CORRECTION)
+	
+	var newPos = Vector2(x*GRID_CORRECTION,y*GRID_CORRECTION)
+	return newPos
+	
