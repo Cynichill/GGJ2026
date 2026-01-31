ae825072e58cf301f5de9885ddbe8dd6af1243cd
Bryn <kyled073@gmail.com>
2026-01-31 14:34:31 +0000

    Player Roles
    Player roles now defined.
    
    Single interaction function changes the output dependant on player role

--- Character/player.gd
+++ Character/player.gd
@@ -6,6 +6,20 @@ const ACCEL = 2.0
 
 var input: Vector2
 
+enum Role {
+	Hunter,
+	Prey
+}
+
+## We have a single interaction key
+## The resulting interaction is dependant on role
+var interactions := {
+	Role.Hunter: func(): trap(),	## Place trap on map
+	Role.Prey: func(): dash()		## Evasive Trap
+}
+
+@export var currentRole = Role.Prey
+
 func get_input():
 	input.x = Input.get_action_strength("Right_%s" % [deviceID]) - Input.get_action_strength("Left_%s" % [deviceID])
 	input.y = Input.get_action_strength("Down_%s" % [deviceID]) - Input.get_action_strength("Up_%s" % [deviceID])
@@ -15,3 +29,16 @@ func _process(delta):
 	var playerInput = get_input()
 	velocity = lerp(velocity, playerInput * SPEED , delta * ACCEL)
 	move_and_slide()
+
+func _input(event: InputEvent):
+	if event.is_action_pressed("Interact_%s" % [deviceID]):
+		interact()
+	
+func interact():
+	interactions[currentRole].call()
+	
+func dash():
+	print("dash")
+	
+func trap():
+	print("trap")
